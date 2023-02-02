871ede1b10187ebdc3eb8631e26a13bdd1f3bf75
Sademm036 <rajasadeem96@gmail.com>
2023-02-02 14:26:06 +0500

    resolving conflicts

--- src/routes/sign-up-route/sign-up-route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const userSignUpRoute = express.Router()
-const userSignuphandler = require('../../controllers/sign-up-controller/sign-up-controller')
-
-
-userSignUpRoute.post("/sign-up", userSignuphandler)
-
-module.exports = userSignUpRoute
-
