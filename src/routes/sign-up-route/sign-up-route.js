77b9768af9fdfaea8af6287c7fdb35a543e06cab
Sademm036 <rajasadeem96@gmail.com>
2023-02-02 14:50:35 +0500

    API's updated

--- src/routes/sign-up-route/sign-up-route.js
+++ src/routes/sign-up-route/sign-up-route.js
@@ -2,9 +2,6 @@ const express = require('express')
 const userSignUpRoute = express.Router()
 const userSignuphandler = require('../../controllers/sign-up-controller/sign-up-controller')
 
-
 userSignUpRoute.post("/sign-up", userSignuphandler)
-
-
 
 module.exports = userSignUpRoute
