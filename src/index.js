61a43639788888fedb9580c4f1406c109e833bc4
Sademm036 <rajasadeem96@gmail.com>
2023-02-02 14:40:48 +0500

    API's updated

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 require('dotenv').config()
 const cors = require('cors')
-
 const userSignUpRoute = require('./routes/sign-up-route/sign-up-route')
+const adminLoginRoute = require ('./routes/admin-login-route/admin-login-route')
 const serviceRoutes = require('./routes/service-info-routes/service-info-routes')
 const complaintRoute = require('./routes/complaint-routes/complaint-routes')
 const feedbackRoute = require('./routes/feedback-route/feedback-route')
 const notificationRoute = require('./routes/notifications-route/notifications-route')
 const settingsRoute = require('./routes/settings-route/settings-route')
 const serviceDetailRoute = require('./routes/service-detail-route/service-detail-route')
+const  userLoginRoute = require ('./routes/log-in-route/log-in-route')
 
 const app = express()
 
@@ -16,6 +17,8 @@ app.use(cors())
 app.use(express.json())
 
 app.use("/user",userSignUpRoute)
+app.use('/users',userLoginRoute)
+app.use('/admin',adminLoginRoute)
 
 
 app.use("/service",serviceRoutes)
