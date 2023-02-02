cd8ff187610accdeb4bf2af102fc888153b28990
Sademm036 <rajasadeem96@gmail.com>
2023-02-02 19:59:45 +0500

    API's updated

--- src/routes/service-info-routes/service-info-routes.js
+++ src/routes/service-info-routes/service-info-routes.js
@@ -3,11 +3,19 @@ const serviceInfoRoutes = express.Router()
 const auth = require('../../middlewares/auth/auth')
 const {
     getServiceCategory,
-    getSubCategory
+    getSubCategory,
+    adminAddServiceCategory,
+    adminDeleteCategoryHandler,
+    adminAddSubCategory,
+    adminDeleteSubCategoryHandler
 } = require('../../controllers/service-info-controllers/service-info-controllers')
 
 serviceInfoRoutes.get("/service-category",auth,getServiceCategory)
 serviceInfoRoutes.get("/sub-category",auth,getSubCategory)
+serviceInfoRoutes.post('/service-category',auth,adminAddServiceCategory)
+serviceInfoRoutes.delete('/service-category',adminDeleteCategoryHandler)
+serviceInfoRoutes.post('/sub-category',auth,adminAddSubCategory)
+serviceInfoRoutes.delete('/sub-category',adminDeleteSubCategoryHandler)
 
 
 
